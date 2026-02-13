0a942b3bb9e4e51f45058a006fb54ba0764a7ced
humamsaf <humamsaf0@gmail.com>
2026-02-13 23:31:16 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Global MBM Dashboard", layout="wide")
+
+st.title("Global Market-Based Mechanisms Dashboard")
+st.write("Dashboard berhasil jalan 🎉")
+
+st.info("Langkah berikutnya: kita akan upload data dan bikin grafik.")
